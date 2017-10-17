7873648045eced8bc9ea48bba0ba12ce20a16eba
Phil Thompson <hello@philthompson.co.uk>
2017-10-17 16:02:28 +0100

    Line breaks FTW

--- src/components/ProjectInvoice.js
+++ src/components/ProjectInvoice.js
@@ -25,7 +25,7 @@ class ProjectInvoice extends Component {
     });
 
     data.subject = 'Invoice for ' + data.project[0].title;
-    data.message = 'Dear ' + data.client[0].contact.name + 'Please find attached an invoice for ' + data.project[0].title;
+    data.message = 'Dear ' + data.client[0].contact.name + "\n\n" + 'Please find attached an invoice for ' + data.project[0].title;
 
     data.day = d.getDate();
     data.month = d.getMonth();
